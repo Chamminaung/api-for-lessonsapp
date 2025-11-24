756e058f66cb252a0605c92a015f2be33f44eb71
Chamminaung <chamminaung25@gmail.com>
2025-11-24 14:28:11 +0630

    Refactor server.js to remove body-parser and enhance JSON request limits

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
@@ -14,7 +13,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -26,7 +27,7 @@ mongoose
   .catch((err) => console.log(err));
 
 
-app.use(bodyParser.json({ limit: "20mb" }));
+
 // OCR endpoint
 app.post("/ocr", async (req, res) => {
   try {
